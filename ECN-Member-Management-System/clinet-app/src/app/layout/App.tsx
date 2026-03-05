bfbc002b69b7bdd7d37dd6d5bd4935cb209b7a2f
ethiopiancommunity2000-sys <ethiopiancommunity2000@gmail.com>
2026-03-05 14:32:14 -0500

    update frontend layout

--- ECN-Member-Management-System/clinet-app/src/app/layout/App.tsx
+++ ECN-Member-Management-System/clinet-app/src/app/layout/App.tsx
@@ -4,6 +4,10 @@ import './style.css';
 import { Outlet, Link as RouterLink } from 'react-router-dom';
 import { Email, Phone, LocationOn } from '@mui/icons-material';
 
+/*
+Frontend: https://jolly-plant-050774f10.6.azurestaticapps.net/
+API: https://ecn-members-api.azurewebsites.net/api
+*/
 //https://chatgpt.com/c/698ab63f-da20-8326-b4cd-7e1c330d6779      --> for azure configration
 
 
